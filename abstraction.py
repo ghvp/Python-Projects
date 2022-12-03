95d89e4b813d86cee505a8fe0ef111b12669b1c4
ghvp <viviennepadilla14@gmail.com>
2022-12-02 21:03:40 -0800

    abstraction submission asignment

---
+++ abstraction.py
@@ -0,0 +1,26 @@
+#importing the needed modules for abstraction
+from abc import ABC, abstractmethod
+#creating the parent class
+class greeting(ABC):
+    def greeting1(self, name):
+        print("Hi {}! How are you doing today?".format(name))
+#abstract method added to greet_per
+    @abstractmethod   
+    def greet_per(self, name):
+        #passing the method since there is no implementation 
+        pass
+
+
+#child class
+class to_who(greeting):
+    def greet_per(self, name):
+        print('This greeting was addressed to {}'.format(name))
+
+
+obj = to_who()
+obj.greeting1('Chris')
+obj.greet_per('Chris')
+        
+
+    
+    
